eb1a11e0ca2ae05c147dbe304b4c55cbe2298883
JuliRed <julianaredondo15@gmail.com>
2021-04-23 21:14:07 -0400

    Configuration of page
    featured rows added
    cells for settings commands

--- SpotOn/Controllers/SettingsViewController.swift
+++ SpotOn/Controllers/SettingsViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: (() -> Void)
+}
+
+
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    private let tableView: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(SettingsTableViewCell.self,
+                       forCellReuseIdentifier: SettingsTableViewCell.identifier)
+        return table
+    }()
+    
+    var models = [SettingsOption]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
+        title = "Settings"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
 
         // Do any additional setup after loading the view.
     }
+    
+    func configure() {
+        self.models = Array(0...100).compactMap( {
+            SettingsOption(title: "Item \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink){
+                
+            }
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingsTableViewCell.identifier,
+            for: indexPath)
+            as? SettingsTableViewCell
+        else {
+                return UITableViewCell()
+            }
+        cell.configure(with: model)
+        return cell
+    }
+    
     
 
     /*

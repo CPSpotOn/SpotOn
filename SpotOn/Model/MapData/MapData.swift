b44f8359eb5fae71b87dbb86d29c896a11e64f2a
Christopher Mena <mena.christopher.student@gmail.com>
2021-05-03 01:31:35 -0400

    added initial files for networking

---
+++ SpotOn/Model/MapData/MapData.swift
@@ -0,0 +1,14 @@
+//
+//  MapData.swift
+//  SpotOn
+//
+//  Created by Christopher Mena on 5/3/21.
+//
+
+import Foundation
+import CoreLocation
+
+struct MapData {
+    let post : [String]
+    let postCoordinates : [CLLocationDegrees]
+}
